0456cdbe167816e103c96e0563d07d1aa1650736
anumsh <anumsharma09@gmail.com>
2017-05-02 11:49:02 -0700

    feat: List example

---
+++ lists/access_list.py
@@ -0,0 +1,7 @@
+# How To Select/Access  An Element From A List
+
+names=["tina","mukal","john","ravi","priya"]
+
+print names      # print all elements in list
+print names[0]   # print tina
+print names[3]   # print ravi
