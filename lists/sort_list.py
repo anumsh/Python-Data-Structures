ad731f5244a6397b7f9c857b4f405c913b1dc9fc
anumsh <anumsharma09@gmail.com>
2017-05-06 23:05:47 -0700

    feat: Sort the list

---
+++ lists/sort_list.py
@@ -0,0 +1,18 @@
+# How To Sort a List in Python
+
+numbers=[56,66,23,12,88,90,3,27]
+
+numbers.sort()  # using sort() method
+
+print numbers  # [3, 12, 23, 27, 56, 66, 88, 90]
+
+
+
+# 2nd method  -   use the sorted() function and pass a list to it
+
+names=["anum","anu","anna","ana","aumm","aman","amna"]
+
+n=sorted(names)
+
+print n   #  ['aman', 'amna', 'ana', 'anna', 'anu', 'anum', 'aumm']
+
