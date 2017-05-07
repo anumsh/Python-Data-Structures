4d8d3714b9e57726aced18ac1ca565719dcfc628
anumsh <anumsharma09@gmail.com>
2017-05-06 23:34:21 -0700

    feat: Count the occurrences of list

---
+++ lists/count_occurences_list.py
@@ -0,0 +1,9 @@
+# How To Count Occurrences Of A List Item In Python
+
+characters=["a","c","n","f","e","h","m","a","m","a","n","e","g"]
+
+# use count()
+
+print characters.count("a")  # 3
+
+print characters.count("n")  # 2
