486a990d1c02b6a46c8042baecf4d755ade2f344
anumsh <anumsharma09@gmail.com>
2017-05-07 23:32:25 -0700

    feat : Add elements in dictionary

---
+++ dictionaries/add_elements_in_dictionary.py
@@ -0,0 +1,15 @@
+# How to change or add elements in a dictionary?
+
+my_dict={'name':'anum','age':20}
+
+# update age
+
+my_dict['age']=22
+
+print my_dict       # {'age': 22, 'name': 'anum'}
+
+# add item
+
+my_dict['address']='CA'
+
+print my_dict       # {'age': 22, 'name': 'anum', 'address': 'CA'}
