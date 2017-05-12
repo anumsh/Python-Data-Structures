c175899b5d65de9d301a8f4c9fb1279151cacd2b
anumsh <anumsharma09@gmail.com>
2017-05-11 23:37:41 -0700

    feat: Add key to dictionary

---
+++ dictionaries/add_key.py
@@ -0,0 +1,9 @@
+# Write a Python program to add a key to a dictionary.
+
+d={"name":"anum","age":20}
+
+print d  # {'age': 20, 'name': 'anum'}
+
+d.update({"favourite_color":"blue"})
+
+print d  # {'age': 20, 'name': 'anum', 'favourite_color': 'blue'}
