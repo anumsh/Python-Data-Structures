0b2bcdff9a99f450bd01e14c8a9f517636a7f804
anumsh <anumsharma09@gmail.com>
2017-05-13 23:28:40 -0700

    feat: multtiply item in dictionary

---
+++ dictionaries/multiply_item_in_dictionary.py
@@ -0,0 +1,10 @@
+# Write a Python program to multiply all the items in a dictionary.
+
+my_dict= {'a':100,'b':200,'c':300}
+
+result=1
+for key in my_dict:
+    result=result * my_dict[key]
+
+print result
+
