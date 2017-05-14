be56e737a65c373a58e6933b125c8e39249f6d07
anumsh <anumsharma09@gmail.com>
2017-05-14 00:11:33 -0700

    feat: Min and Max value in a dictionary

---
+++ dictionaries/min_max_in_dictinary.py
@@ -0,0 +1,10 @@
+# Write a Python program to get the maximum and minimum value in a dictionary.
+
+
+my_dict={'x':300,'y':490,'z':900}
+
+key_max = max(my_dict.keys())
+key_min = min(my_dict.keys())
+
+print ('maximum value :',my_dict[key_max])
+print ('minimum value: ',my_dict[key_min])
