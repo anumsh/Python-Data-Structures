0b6ab6648ec09f96aa8a2bd66425840308e7feec
anumsh <anumsharma09@gmail.com>
2017-05-15 23:24:26 -0700

    feat: Create dictionary from a string

---
+++ dictionaries/create_dictionary_from_string.py
@@ -0,0 +1,10 @@
+# Write a Python program to create a dictionary from a string.
+
+
+
+string1='anum8sharma'
+my_dict={}
+for letter in string1:
+    my_dict[letter]=my_dict(letter, 0) + 1
+
+print (my_dict)
